b0445196f61a00e9c457223d18da9c9f3feb5824
cloe001000 <69475099+cloe001000@users.noreply.github.com>
2021-02-24 11:42:48 +0900

    서버의 통신망 구축 시스템 실험 성공

--- env.py
+++
@@ -1,6 +0,0 @@
-SERVER_IP: str = "112.158.160.11"
-SERVER_PRIVATE_IP: str = "192.168.219.101"
-PORT_NUMBER: int = 55555
-# ? 보통 1024의 배수가 네트워크 버퍼 크기랑 잘 맞는다
-BUF_SIZE: int = 1024
-SECRET_CODE: bytes = "수신 성공!".encode("utf-8")
